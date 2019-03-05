b4a140054afc72c6e665780de21a8cf17ededdc1
joel burton <joeltburton@gmail.com>
2019-03-05 13:42:10 -0800

    update error handler, page not found

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const db = require('./models/db'); // import triggers db connection
-
 const tripController = require('./models/trip/tripController');
-
 
 const app = express();
 const port = process.env.PORT;
@@ -16,12 +14,16 @@ app.get('/test', tripController.findAllTrips, (req, res) => res.send('test'));
 app.post('/test', tripController.createTrip, (req, res) => res.redirect('/test'));
 
 
+app.use((req, res) => res.status(400).send('sorry, we couldn\'t find that page...'));
+
 app.use((err, req, res, next) => {
   // database errors
   if (err.db) {
     console.error('database error:', err.db);
-    res.status(500).send(err.message);
+    return res.status(500).send(err.message);
   }
+
+  return res.status(500).send('sorry, something broke...');
 });
 
 app.listen(port, () => console.log(`\n📡  server listening on port ${port}...`));
