5d591bb323bb813036edf11d98fee2c42a347416
jordanzobean <jordanbetzer@gmail.com>
2019-03-05 17:08:57 -0800

    created Spalsh, WaypointCard, WaypointNodes and MapPage components

---
+++ client/src/app/components/WaypointCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WaypointCard = props => {
+  return (
+    <div>
+      <span>Location Name</span>
+      <span>Date</span>
+      <img />
+    </div>
+  );
+};
--- client/src/app/containers/MapPage.jsx
+++ client/src/app/containers/MapPage.jsx
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class MapPage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
-  render() {
-    return <div>Hello World</div>;
-  }
 }
-
-export default App;
